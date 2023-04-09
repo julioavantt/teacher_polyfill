ac80ca35914e3b1292e59b30aa92774e93d53d53
Isabela <codingishappiness@byjulio.com>
2023-04-09 09:04:14 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import "core-js"
+
+class Test {
+    #firstName = 'test-name';
+  }
+  
+  const test = new Test();
+  console.log(test.firstName);
+
+  // https://dev.to/brayanarrieta/new-javascript-features-ecmascript-2022-with-examples-4nhg
